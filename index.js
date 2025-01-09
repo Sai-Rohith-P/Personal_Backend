7ad437c84ea3e56af47651e97c2c07d706f1694b
rohith <rohithsai4734@gmail.com>
2025-01-09 13:56:01 +0530

    addedfronendlink

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 const app = express()
 
 app.use(cors());
-app.use(cors({ origin: '*' }))
+app.use(cors({ origin: 'https://personal-frontend-rgfa.onrender.com' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 ConnectingDB()
